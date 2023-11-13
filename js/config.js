89ddb0fcdcffa3a130dcbf5367973d3a5191e539
sodaski5 <sodaski5@gmail.com>
2023-11-13 22:27:03 +0100

    new_config0

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-central-1_02hEWsMXS',
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolClientId: '25ddkmj4v6hfsfvruhpfi7n4hv',
+        region: 'eu-central-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
