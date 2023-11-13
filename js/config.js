9037f4136b59d088deb7eff24edaeee64842d475
sodaski5 <sodaski5@gmail.com>
2023-11-13 23:42:47 +0100

    new-configurations

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-central-1'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://brvi1w9yi9.execute-api.eu-central-1.amazonaws.com/prod'
     }
 };
